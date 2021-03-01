8753f1a2fc68cd6fb8f786c9c4122ee73f0fcacb
Antonio Venditti <vasoft83@gmail.com>
2021-03-01 17:52:10 +0100

    clear Code

--- CorsoWebApi/CorsoWebApi/Controllers/EventiController.cs
+++ CorsoWebApi/CorsoWebApi/Controllers/EventiController.cs
@@ -25,17 +25,12 @@ namespace CorsoWebApi.Controllers
         public override IActionResult Get(int id) => base.Get(id);
 
         [HttpPost]
-        public override IActionResult Post([FromBody]
-            Evento evento) => base.Post(evento);
+        public override IActionResult Post([FromBody] Evento evento) => base.Post(evento);
 
         [HttpPut("{id}")]
-        public override IActionResult Put([FromRoute]
-            int id, [FromBody]
-            Evento evento)
-            => base.Put(id, evento);
+        public override IActionResult Put([FromRoute] int id, [FromBody] Evento evento) => base.Put(id, evento);
 
         [HttpDelete("{id}")]
-        public override IActionResult Delete([FromRoute]
-            int id) => base.Delete(id);
+        public override IActionResult Delete([FromRoute] int id) => base.Delete(id);
     }
 }
